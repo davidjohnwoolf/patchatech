1aeb9c64d0cc92454be3f4d86a9d4959716a34fe
David Woolf <david@astaroth.domain.actdsltmp>
2014-07-07 16:22:57 -0600

    generate site controller, add index to site views

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
 
-  describe 'associations', :focus do
+  describe 'associations' do
     it { should have_many :tutorials }
   end
 
