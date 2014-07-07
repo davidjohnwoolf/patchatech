8c9bd444e026e10950c17593b4df4a1a50aa77f4
LandonWest <landonwest5@gmail.com>
2014-07-07 16:46:12 -0600

    update tutorial.rb and tutorial_spec.rb

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'associations' do
+    it { should belong_to :user }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :user_id :title :description :category }
+  end
+
 end
