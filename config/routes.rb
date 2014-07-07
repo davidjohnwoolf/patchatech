d042968054fba8a5cba8cff355a2b052961ab278
David Woolf <david@astaroth.domain.actdsltmp>
2014-07-07 16:34:58 -0600

    add resources :tutorials to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :tutorials
   devise_for :users
   root 'site#index'
 
--- spec/controllers/site_controller_spec.rb
+++ spec/controllers/site_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe SiteController, :type => :controller do
 
+  describe '#index', :focus do
+    it 'successfully renders site index' do
+      get :index
+      expect(response).to be_success
+      expect(response).to render_template('index')
+    end
+  end
+
 end
