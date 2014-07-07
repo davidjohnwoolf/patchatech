afdbacf6e55ab04d0667315f627bd04b249ba423
David Woolf <david@astaroth.domain.actdsltmp>
2014-07-07 16:43:58 -0600

    remove focus from site tests

--- spec/controllers/site_controller_spec.rb
+++ spec/controllers/site_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe SiteController, :type => :controller do
 
-  describe '#index', :focus do
+  describe '#index' do
     it 'successfully renders site index' do
       get :index
       expect(response).to be_success
