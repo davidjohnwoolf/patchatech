c82a97a87d7d6bed37b74bbe6e3cf4525bb721cf
David Woolf <david@astaroth.domain.actdsltmp>
2014-07-08 16:02:22 -0600

    add custom devise registration controller and update devise route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   resources :tutorials
-  devise_for :users
+
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
   root 'site#index'
 
 end
