a0f2b8e903022ab4d5912f8ee1b1dec76a89e710
David Woolf <david@astaroth.domain.actdsltmp>
2014-07-08 16:17:34 -0600

    add user_id to the create action in tutorials controller

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -14,6 +14,7 @@ class TutorialsController < ApplicationController
 
     def create
       @tutorial = Tutorial.new(tutorial_params)
+      @tutorial.user_id = current_user.id
       if @tutorial.save
         redirect_to tutorials_path
       else
