ce16797ece753de9d8c57c3538c65cc8a1429c9a
LandonWest <landonwest5@gmail.com>
2014-07-09 17:52:34 -0600

    update user_controller validation, add edit link to user show page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,7 +2,7 @@ class UserController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = User.find(params[:id])
+      @user = User.find(params[:id])
   end
 
 end
