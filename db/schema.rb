3e081f6a8db8e7798cfb0894a331e3df44131411
LandonWest <landonwest5@gmail.com>
2014-07-13 16:30:59 -0600

    add_picture_to_users migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140712223708) do
+ActiveRecord::Schema.define(version: 20140712233617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20140712223708) do
     t.string   "username",                            null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "picture"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
