901ec33b146cd241dffd88ee95e0185ea1d74b40
Skewampus <herrogoodbye24@gmail.com>
2014-07-14 16:41:20 -0600

    add in video to tutorial factory

--- spec/factories/tutorials.rb
+++ spec/factories/tutorials.rb
@@ -5,5 +5,7 @@ FactoryGirl.define do
     title { Faker::Lorem.word }
     description { Faker::Lorem.sentence(word_count = 10) }
     category { Faker::Lorem.word }
+    video { Rack::Test::UploadedFile.new(File.join(Rails.root, 'public', 'uploads', 'tutorial', 'video', '5', 'Responsive_Web_Design_with_Foundation_-_1.1._Introduction.mp4')) }
+
   end
 end
