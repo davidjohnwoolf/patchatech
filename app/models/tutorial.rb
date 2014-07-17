1d7376cc81a561758f3be6937dfc1a7fb51d5263
LandonWest <landonwest5@gmail.com>
2014-07-16 18:00:43 -0600

    remove video-tile validation for tutorials

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,7 +1,7 @@
 class Tutorial < ActiveRecord::Base
 
   belongs_to :user
-  validates :user_id, :title, :description, :category, :video, :video_tile, presence: true
+  validates :user_id, :title, :description, :category, :video, presence: true
   mount_uploader :video_tile, VideoTileUploader
   mount_uploader :video, TutorialVideoUploader
 end
