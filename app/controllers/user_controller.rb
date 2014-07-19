0cdeeff558913c43b292ebc3c83936f0c40c56fe
LandonWest <landonwest5@gmail.com>
2014-07-19 14:14:13 -0600

    add pagination to user profile tutorials

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,7 +3,7 @@ class UserController < ApplicationController
 
   def show
       @user = User.find(params[:id])
-      @tutorials = Tutorial.where(user_id: params[:id])
+      @tutorials = Tutorial.where(user_id: params[:id]).page(params[:page]).per(12)
   end
 
 end
