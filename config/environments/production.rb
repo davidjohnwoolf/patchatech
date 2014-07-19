751e7944cc678b5854bb2bb7e79aaf6d7fa5e5c0
David Woolf <davidjohnwoolf@gmail.com>
2014-07-19 15:27:50 -0600

    configure production.rb: set environment variables for domain name and email, configure email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,25 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  ###------ From Quintin: setting up confirmable email stuff
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV['DOMAIN_NAME'],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => ENV['DOMAIN_NAME'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+
+  ###------
+
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
