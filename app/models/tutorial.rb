f0c3dae9e84c65c334238bcf3920317d96613674
Skewampus <herrogoodbye24@gmail.com>
2014-07-19 12:17:29 -0600

    add search self.search method

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,8 +2,35 @@ class Tutorial < ActiveRecord::Base
   include Elasticsearch::Model
 
   mapping do
-    indexes :title, type: 'string'
+    indexes :title, type: 'string', analyzer: 'simple' #, fuzziness: 2, completion: 'suggest'
     indexes :description
+  end
+
+  def self.search(params, options={})
+
+     es = __elasticsearch__
+       definition = {
+         query: {
+           query_string:{
+             query: params
+
+          }
+        }
+      }
+
+
+
+        # definition.merge!({
+        #   fuzzy: {
+        #     tutorial: {
+        #       value: params,
+        #       fuzziness: 2
+        #     }
+        #   }
+        # })
+
+      response = es.search(definition)
+      response
   end
 
 
