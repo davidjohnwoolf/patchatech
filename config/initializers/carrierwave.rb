28c761da306a0f923870387525559aff1d2548f8
Skewampus <herrogoodbye24@gmail.com>
2014-07-23 15:50:04 -0600

    remove access policy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -29,6 +29,6 @@ CarrierWave.configure do |config|
   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
 
   config.fog_directory    = ENV['S3_BUCKET_NAME']
-  config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
+  # config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
   config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
 end
