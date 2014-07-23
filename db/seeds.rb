d17a1984d8c7b8c0137e26a06a96bacbc9567e63
LandonWest <landonwest5@gmail.com>
2014-07-23 16:55:23 -0600

    add tutorials in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+25.Times do
+
+  Tutorial.create({
+    user_id: 1
+    title: Faker::Ipsum.words(num = 20)
+    description: Faker::Ipsum.words(num = 20)
+    category: "computer"
+    video: "/Users/landonwest/Desktop/Responsive_Web_Design_with_Foundation_-_1.1._Introduction.mp4"
+    })
+
+end
