dbf440c3a5230f34f4925b22972a6c4000789493
LandonWest <landonwest5@gmail.com>
2014-07-24 13:25:41 -0600

    add spec to site_controller

--- spec/controllers/site_controller_spec.rb
+++ spec/controllers/site_controller_spec.rb
@@ -19,11 +19,26 @@ RSpec.describe SiteController, :type => :controller do
   end
 
   describe '#contact' do
-    it 'successfully renders site index' do
-      get :contact
-      expect(response).to be_success
-      expect(response).to render_template('contact')
+    context 'when the user goes to the contact page' do
+      it 'successfully renders site contact' do
+        get :contact
+        expect(response).to be_success
+        expect(response).to render_template('contact')
+      end
     end
+    context 'when it is a post request' do
+      # before do
+      #   params = { user_name: "user_name", comment: "comment", user_email: "user_email" }
+      #   @mailer = ContactMailer.contact_mailer(params)
+      # end
+      it 'successfully renders site index' do
+        post :contact
+        expect(response).to be_success
+        expect { @mailer.contact_mailer }.to change { ActionMailer::Base.deliveries }.by(1)
+        expect(response).to redirect_to root_path
+      end
+    end
+
   end
 
 end
