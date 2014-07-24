9f6ceb6302ebb7d4f1b91e5520e612393be229c3
David Woolf <davidjohnwoolf@gmail.com>
2014-07-24 15:23:41 -0600

    reverse changes to user_controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,6 @@
 class UserController < ApplicationController
   before_action :authenticate_user!
-  
+
   def show
       @user = User.find(params[:id])
       @tutorials = Tutorial.where(user_id: params[:id]).page(params[:page]).per(12)
