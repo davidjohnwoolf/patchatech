c23586a0c767aedd63de375e4c9564ced5f9ef0a
Skewampus <herrogoodbye24@gmail.com>
2014-07-24 12:04:34 -0600

    add mailer testing

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -1,5 +1,37 @@
 require "rails_helper"
+require 'pry'
 
 RSpec.describe ContactMailer, :type => :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#contact_mailer' do
+    before do
+      @user = { name: 'test', comments:'testing comments', email: 'test@test.com' }
+      @mail = ContactMailer.contact_mailer(@user)
+
+    end
+    it 'renders Contact Request' do
+      expect(@mail.subject).to eql('Contact Request')
+    end
+
+    it 'renders the sender email' do
+      expect(@mail.from).to eql(['contact-form@patchatech.com'])
+    end
+
+    it 'renders the receiver email' do
+      expect(@mail.to).to include('patchatech@gmail.com')
+    end
+
+    it 'assigns user_name' do
+      expect(@mail.body.encoded).to match('test')
+    end
+
+    it 'assigns comments' do
+      expect(@mail.body.encoded).to match('testing comments')
+    end
+
+    it 'assigns emails' do
+      expect(@mail.body.encoded).to match('test@test.com')
+    end
+
+  end
 end
