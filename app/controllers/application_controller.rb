81d9f12fe39c14333340d67703cabb22b88faf78
David Woolf <davidjohnwoolf@gmail.com>
2014-07-24 15:47:15 -0600

    removed edit tutorial modal on layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def set_modal
     @new_tutorial = Tutorial.new
-    @this_tutorial = Tutorial.find(params[:id]) if params[:id] && params(:controller).eql?('tutorial')
+    # @this_tutorial = Tutorial.find(params[:id]) if params[:id]
     @user = User.new
   end
 
