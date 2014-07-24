28acc65526948265a8dcaec8dd6dbb47f2bdbba8
LandonWest <landonwest5@gmail.com>
2014-07-24 10:54:04 -0600

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-25.Times do
-
+10.times do
   Tutorial.create({
-    user_id: 1
-    title: Faker::Ipsum.words(num = 20)
-    description: Faker::Ipsum.words(num = 20)
-    category: "computer"
-    video: "/Users/landonwest/Desktop/Responsive_Web_Design_with_Foundation_-_1.1._Introduction.mp4"
+    user_id: 1,
+    title: Faker::Lorem.words(num = 20),
+    description: Faker::Lorem.words(num = 20),
+    category: "computer",
     })
-
 end
