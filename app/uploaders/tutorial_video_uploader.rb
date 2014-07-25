4c157b75425e9eaf22fa92b9b5374534d8910305
Skewampus <herrogoodbye24@gmail.com>
2014-07-25 12:00:36 -0600

    fix uploading to s3 on tests

--- app/uploaders/tutorial_video_uploader.rb
+++ app/uploaders/tutorial_video_uploader.rb
@@ -7,7 +7,7 @@ class TutorialVideoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-   storage :aws
+  # storage :aws
   # storage :file
 
   # Override the directory where uploaded files will be stored.
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,15 @@
 CarrierWave.configure do |config|
-  config.storage    = :aws
-  config.aws_bucket = ENV['S3_BUCKET_NAME']
-  config.aws_acl    = :public_read
-  #config.asset_host = 'http://example.com'
-  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
-
-  config.aws_credentials = {
-    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-  }
+  if Rails.env == 'test'
+    config.storage = :file
+  else
+    config.storage = :aws
+    config.aws_bucket = ENV['S3_BUCKET_NAME']
+    config.aws_acl    = :public_read
+    #config.asset_host = 'http://example.com'
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
+    config.aws_credentials = {
+      access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  end
 end
