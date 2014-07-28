3b95cdbf55e0c65fa167fd550a560ca3787aaa66
Skewampus <herrogoodbye24@gmail.com>
2014-07-28 16:33:30 -0600

    remove previous change

--- config/initializers/configinitializerbonsai.rb
+++ config/initializers/configinitializerbonsai.rb
@@ -1,11 +1,1 @@
-#Elasticsearch::Model.client = Elasticsearch::Client.new logs: true, url: ENV['BONSAI_URL']
-
-require 'elasticsearch/model'
-
-if ENV['BONSAI_URL']
-  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
-  BONSAI_INDEX_NAME = ENV['BONSAI_URL'][/[^\/]+$/]
-else
-  app_name = Rails.application.class.parent_name.underscore.dasherize
-  BONSAI_INDEX_NAME = "#{app_name}-#{Rails.env}"
-end
+Elasticsearch::Model.client = Elasticsearch::Client.new logs: true, url: ENV['BONSAI_URL']
