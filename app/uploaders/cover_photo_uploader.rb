1adf93bf4683fb16fc1f2139cdc7754dc014be2d
Skewampus <herrogoodbye24@gmail.com>
2014-07-28 17:40:55 -0600

    added default cover file / normal size for cover photo

--- app/uploaders/cover_photo_uploader.rb
+++ app/uploaders/cover_photo_uploader.rb
@@ -33,6 +33,10 @@ class CoverPhotoUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [200, 160]
   end
 
+  version :normal do
+    process :resize_to_fill => [200, 160]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
