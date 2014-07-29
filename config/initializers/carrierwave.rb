cd65e6eefbeedcd21282269f38d01ee567f764b6
David Woolf <davidjohnwoolf@gmail.com>
2014-07-28 18:38:41 -0600

    remove method in carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,5 +12,4 @@ CarrierWave.configure do |config|
       access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
-    config.remove_previously_stored_files_after_update = false
 end
