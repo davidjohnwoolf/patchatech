b9b578c2fab75bc526cfe80a2346166f86513cb9
Skewampus <herrogoodbye24@gmail.com>
2014-07-29 14:15:34 -0600

    reverting tutorial

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,49 +1,49 @@
 class Tutorial < ActiveRecord::Base
-  # include Elasticsearch::Model
-  #include Elasticsearch::Model::Callbacks
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 
-  # mapping do
-  #   indexes :title, type: 'string', analyzer: 'simple' #, fuzziness: 2, completion: 'suggest'
-  #   indexes :description
-  # end
-  # validates :user_id, :title, :description, :category, presence: true
+  mapping do
+    indexes :title, type: 'string', analyzer: 'simple' #, fuzziness: 2, completion: 'suggest'
+    indexes :description
+  end
+  validates :user_id, :title, :description, :category, presence: true
   mount_uploader :cover_photo, CoverPhotoUploader
-  # mount_uploader :video, TutorialVideoUploader
+  mount_uploader :video, TutorialVideoUploader
 
   belongs_to :user
 
-  # def self.search(params, options={})
-  #    es = __elasticsearch__
-  #      definition = {
-  #        query: {
-  #          query_string:{
-  #            query: params
-  #
-  #         }
-  #       }
-  #     }
-  #
-  #     response = es.search(definition)
-  #     response
-  # end
+  def self.search(params, options={})
+     es = __elasticsearch__
+       definition = {
+         query: {
+           query_string:{
+             query: params
 
-  # def cover_photo_changed?
-  #   changed.include?("cover_photo")
-  # end
+          }
+        }
+      }
 
-  # def as_indexed_json(opts={})
-  #   as_json(
-  #     only: [
-  #       :title,
-  #       :description,
-  #       :category
-  #     ],
-  #     methods: [
-  #       :cover_photo,
-  #       :video,
-  #       :video_duration
-  #     ]
-  #   )
-  # end
+      response = es.search(definition)
+      response
+  end
+
+  def cover_photo_changed?
+    changed.include?("cover_photo")
+  end
+
+  def as_indexed_json(opts={})
+    as_json(
+      only: [
+        :title,
+        :description,
+        :category
+      ],
+      methods: [
+        :cover_photo,
+        :video,
+        :video_duration
+      ]
+    )
+  end
 
 end
