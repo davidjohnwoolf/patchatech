2a6c16562c2e026120d709b89a9791e8386a466c
LandonWest <landonwest5@gmail.com>
2016-03-28 20:53:35 -0700

    update carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,8 +9,8 @@ CarrierWave.configure do |config|
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'us-west-1',
-      host: 's3.amazonaws.com'
-      # endpoint: 'https://s3-us-west-1.amazonaws.com'
+      host: 's3.amazonaws.com',
+      endpoint: 'https://s3-us-west-1.amazonaws.com'
     }
     config.fog_directory = ENV['S3_BUCKET_NAME']
     config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
